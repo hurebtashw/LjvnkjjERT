7636f7e07c0ce03422a0687469ae7ce7e42f05db
hurebtashw <104347815+hurebtashw@users.noreply.github.com>
2022-09-02 17:23:28 +0300

    Update better_winlite.h

--- better_winlite.h
+++ better_winlite.h
@@ -7,7 +7,6 @@
 
 #define NOWINRES
 #define NOSERVICE
-#define NOMCX
 #define NOIME
 #define NOCRYPT
 #define NOMETAFILE
@@ -16,18 +15,15 @@
 
 #include <windows.h>
 
-#undef PostMessage
 #undef CreateEvent
 #undef PlaySound
 #undef SetCursor
 #undef ProgressBox
-#undef RegisterClass
 #undef AddJob
 #undef GetJob
 #undef Yield
 #undef SetPort
 #undef CreateFont
-#undef ShellExecute
 #undef ShellExecuteEx
 #undef GetCurrentTime
 #undef GetTickCount
